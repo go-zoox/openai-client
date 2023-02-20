6fffd4aeb15d857e5a0762eee01ce30f0eac248d
whatwewant <tobewhatwewant@outlook.com>
2023-02-20 09:01:39 +0800

    chore(completion): default Temperature 0.8, which is better than 1

--- completion.go
+++ completion.go
@@ -2,13 +2,35 @@ package openaiclient
 
 // CreateCompletionRequest ...
 type CreateCompletionRequest struct {
-	// Model is ID of the model to use. You can use the List models API to see all of your available models, or see our Model overview for descriptions of them.
-	Model       string `json:"model"`
-	Prompt      string `json:"prompt"`
-	MaxTokens   int    `json:"max_tokens"`
-	Temperature int    `json:"temperature"`
-	Stream      bool   `json:"stream"`
-	Stop        string `json:"stop"`
+	// Model is ID of the model to use.
+	// You can use the List models API to see all of your available models,
+	// or see our Model overview for descriptions of them.
+	// model is required.
+	Model string `json:"model"`
+
+	// prompt is the prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays.
+	// Note that <|endoftext|> is the document separator that the model sees during training,
+	// so if a prompt is not specified the model will generate as if from the beginning of a new document.
+	Prompt string `json:"prompt"`
+
+	// MaxTokens is the maximum number of tokens to generate in the completion.
+	// The token count of your prompt plus max_tokens cannot exceed the model's context length.
+	// Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+	MaxTokens int `json:"max_tokens"`
+
+	// Temperature means What sampling temperature to use, between 0 and 2.
+	// Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+	// We generally recommend altering this or top_p but not both.
+	Temperature float64 `json:"temperature"`
+
+	// Stream whether to stream back partial progress.
+	// If set, tokens will be sent as data-only server-sent events as they become available, with the stream terminated by a data: [DONE] message.
+	Stream bool `json:"stream"`
+
+	// Stop is up to 4 sequences where the API will stop generating further tokens.
+	// The returned text will not contain the stop sequence.
+	Stop string `json:"stop"`
+
 	// Suffix      string `json:"suffix"`
 	// TopP        int    `json:"top_p"`
 	// N           int    `json:"n"`
@@ -35,6 +57,10 @@ type CreateCompletionResponse struct {
 
 // reference: https://platform.openai.com/docs/api-reference/completions/create
 func (c *client) CreateCompletion(cfg *CreateCompletionRequest) (*CreateCompletionResponse, error) {
+	if cfg.Temperature == 0 {
+		cfg.Temperature = 0.8
+	}
+
 	resp, err := c.post("/v1/completions", cfg)
 	if err != nil {
 		return nil, err
